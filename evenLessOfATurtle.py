dec733e66f2606045f89f8f578d94d0d1ae6d272
Gerard Whitehead <gmwhitehead@gmail.com>
2022-02-22 22:00:39 +0000

    Teach betterTurtle how to draw hypotrochioids

--- evenLessOfATurtle.py
+++ evenLessOfATurtle.py
@@ -1,5 +1,8 @@
 import turtle
 import random
+from math import sin
+from math import cos
+from math import radians
 # The below command makes the turtle module accept a sequence of ints (rather than floats)
 # as RGB colour selection.
 turtle.colormode(255)
@@ -44,16 +47,42 @@ class betterTurtle(turtle.Turtle):
             
     def totallyRandomColour(self):
         totallyRandomColour = (random.randrange(0, 255),  random.randrange(0, 255),  random.randrange(0, 255))
-        print("Totally random colour, Red: %d, Green: %d, Blue: %d" % totallyRandomColour)
+        #print("Totally random colour, Red: %d, Green: %d, Blue: %d" % totallyRandomColour)
         return totallyRandomColour
         
     def randomPenColour(self):
         self.color(self.totallyRandomColour())
+        
+        
+    def plotSomethingCool(self,  degrees=360,  bigCogSize=250,  littleCogSize=55,  diameter = 125,  accuracy=3):
+        ## Every statement in here featuring 'wn' is to speed up the drawing a bit.
+        ## It will still work if you remove _all_ of them, but be a lot slower.
+        self.pensize(1.5)
+        wn = turtle.Screen()
+        wn.tracer(0)
+        self.penup()
+        self.home()
+        for i in range(0,  degrees,  accuracy):
+            # If we've not moved yet, leave the pen up.
+            if i == 0:
+                self.penup()
+            else:
+                self.pendown()
+            # Assume we've just plotted the previous point, 'cos it doesn't matter too much if we have.
+            angle=radians(i)
+            R = bigCogSize
+            r = littleCogSize
+            d = diameter
+            x = (R - r) * cos(angle) + d * cos(((R-r)/r)*angle)
+            y = (R - r) * sin(angle) - d * sin(((R-r)/r)*angle)
+            self.color(self.totallyRandomColour())
+            self.goto(x, y)
+            wn.update()
+            
 
 
 
 bob = betterTurtle("Bob")
-bob.speed(200)
 
 # Get bob to draw a rhombus
 #bob.walkARhombus()
@@ -67,10 +96,22 @@ bob.speed(200)
 #bob.spirograph()
 
 # Maybe with some random shapes and sizes... ?
-bob.spirograph(random.randrange(1, 10),  random.randrange(1,  500), random.randrange(10))
+#bob.spirograph(random.randrange(1, 10),  random.randrange(1,  500), random.randrange(10))
 
 # Tell bob to draw an arbitrary regular polygon (default inputs)
-bob.walkRegularPolygon()
+#bob.walkRegularPolygon()
 
 # Tell bob to draw an abitrary regular polygon (with values we give him!)
-bob.walkRegularPolygon(22, 100)
+#bob.walkRegularPolygon(7, 60)
+
+bob.speed(500)
+
+#bob.plotSomethingCool(3600)
+
+# show off 20 different random ones
+bob.hideturtle()
+for i in range(20): 
+    bob.clear()
+    bigCogSize=random.randrange(40, 300)
+    smallCogSize=random.randrange(40, 200)
+    bob.plotSomethingCool(36000,  bigCogSize,  smallCogSize,  250 , 3)
